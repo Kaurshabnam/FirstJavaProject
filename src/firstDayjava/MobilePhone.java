25c6f713d9be8d5fea2ce5d5195b323bbae6cc23
Shabnam <shabnamkaur72@gmail.com>
2024-04-04 20:34:01 -0400

    my first commit

---
+++ src/firstDayjava/MobilePhone.java
@@ -0,0 +1,11 @@
+package firstDayjava;
+
+public class MobilePhone {
+ //DataType VariableName= Value
+	 // byte Int, float, double, long
+	int Memory = 8;
+	 
+	 double screenSize = 6.5;
+	 
+	 
+}
